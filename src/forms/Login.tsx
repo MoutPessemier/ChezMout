1053b5d6649f4474ddca571543c06422b0ccd417
Mout Pessemier <moutpessemier@hotmail.com>
2023-04-20 21:31:34 +0200

    [FEAT] added login and register forms

---
+++ src/forms/Login.tsx
@@ -0,0 +1,39 @@
+import { useFormik } from "formik";
+import { FC } from "react";
+import * as Yup from 'yup';
+import TextField from "@/components/TextField/TextField.component";
+import Button from "@/components/Button/Button.component";
+
+
+interface LoginValues {
+	email: string;
+	password: string;
+}
+
+const LoginSchema = Yup.object().shape({
+	email: Yup.string().email('Invalid email!').required('Required!'),
+	password: Yup.string().required('Required!')
+})
+
+const RegisterForm: FC = ({ ...props }) => {
+
+	const formik = useFormik({
+		initialValues: {
+			email: '',
+			password: ''
+		},
+		validationSchema: LoginSchema,
+		onSubmit: values => { onSubmitHandler(values) }
+	});
+
+	const onSubmitHandler = (values: LoginValues) => {
+		console.log(values);
+	}
+
+	return <form onSubmit={formik.handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+		<TextField label="Email" id="email" type="email" {...formik.getFieldProps('email')} />
+		<TextField label="Password" id="password" type="password" {...formik.getFieldProps('password')} />
+		<Button text="Submit" />
+	</form>
+};
+
---
+++ src/forms/Register.tsx
@@ -0,0 +1,51 @@
+import { useFormik } from "formik";
+import { FC } from "react";
+import * as Yup from 'yup';
+import TextField from "@/components/TextField/TextField.component";
+import Button from "@/components/Button/Button.component";
+
+
+interface RegisterValues {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+}
+
+const RegisterSchema = Yup.object().shape({
+	firstName: Yup.string().min(2).max(20).required('Required!'),
+	lastName: Yup.string().min(2).max(20).required('Required!'),
+	email: Yup.string().email('Invalid email!').required('Required!'),
+	password: Yup.string().required('Required!'),
+	confirmPassword: Yup.string().required('Required!')
+})
+
+const RegisterForm: FC = ({ ...props }) => {
+
+	const formik = useFormik({
+		initialValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			password: '',
+			confirmPassword: ''
+		},
+		validationSchema: RegisterSchema,
+		onSubmit: values => { onSubmitHandler(values) }
+	});
+
+	const onSubmitHandler = (values: RegisterValues) => {
+		console.log(values);
+	}
+
+	return <form onSubmit={formik.handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+		<TextField label="First Name" id="firstName" {...formik.getFieldProps('firstName')} />
+		<TextField label="Last Name" id="lastName" {...formik.getFieldProps('lastName')} />
+		<TextField label="Email" id="email" type="email" {...formik.getFieldProps('email')} />
+		<TextField label="Password" id="password" type="password" {...formik.getFieldProps('password')} />
+		<TextField label="Confirm Password" id="confirmPassword" type="password" {...formik.getFieldProps('confirmPassword')} />
+		<Button text="Submit" />
+	</form>
+};
+
